e502e0d40ffbf12588a0088780dbca42ddafe626
arief <arief25ramadhan@yahoo.com>
2024-05-14 21:32:47 +0700

    tidying code

--- point_cloud_starter.py
+++ point_cloud_starter.py
@@ -18,17 +18,12 @@ print(f"Points after downsampling: {len(pcd.points)}")# DOWNSAMPLING
 # open3d.visualization.draw_geometries([pcd])
 
 # ## CHALLENGE 3 - SEGMENTATION: Using RANSAC (Random Sampling Consensus), to differentiate between ground and obstacles
-start_time = time.time()
-
 plane_model, inliers = pcd.segment_plane(distance_threshold=0.3, ransac_n=3, num_iterations=150)
 inlier_cloud = pcd.select_by_index(inliers)
 inlier_cloud.paint_uniform_color([0, 1, 1])
 outlier_cloud = pcd.select_by_index(inliers, invert=True)
 outlier_cloud.paint_uniform_color([1, 0, 0])
 # open3d.visualization.draw_geometries([inlier_cloud, outlier_cloud])
-
-elapsed_time = time.time() - start_time
-print("Elapsed time: {}".format(elapsed_time))
 
 # ## CHALLENGE 4 - CLUSTERING USING DBSCAN
 with open3d.utility.VerbosityContextManager(open3d.utility.VerbosityLevel.Debug) as cm:
