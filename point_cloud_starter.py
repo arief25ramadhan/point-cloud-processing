2bf4ebbc0f5d7636fc2b5234545129c5a2b8013f
Arief Ramadhan <arief25ramadhan@yahoo.com>
2024-06-11 14:21:27 +0700

    Update point_cloud_starter.py

--- point_cloud_starter.py
+++ point_cloud_starter.py
@@ -1,4 +1,4 @@
-## 0. Import Library
+# 0. Import Library
 import numpy as np
 import time
 import open3d
@@ -14,24 +14,26 @@ pcd = pcd.voxel_down_sample(voxel_size=0.1)
 print(f"Points after downsampling: {len(pcd.points)}")
 # open3d.visualization.draw_geometries([pcd])
 
-# 3. RANSAC Segmentation
+# 3. RANSAC Segmentation to identify the largest plane (in this case the ground/road) from objects above it
 plane_model, inliers = pcd.segment_plane(distance_threshold=0.3, ransac_n=3, num_iterations=150)
-## Identify inlier points
+## Identify inlier points -> road
 inlier_cloud = pcd.select_by_index(inliers)
 inlier_cloud.paint_uniform_color([0, 1, 1])
-## Identify outlier points
+## Identify outlier points -> objects on the road
 outlier_cloud = pcd.select_by_index(inliers, invert=True)
 outlier_cloud.paint_uniform_color([1, 0, 0])
 # open3d.visualization.draw_geometries([inlier_cloud, outlier_cloud])
 
-# 4. Clustering using DBSCAN
+# 4. Clustering using DBSCAN -> To further segment objects on the road
 with open3d.utility.VerbosityContextManager(open3d.utility.VerbosityLevel.Debug) as cm:
     labels = np.array(outlier_cloud.cluster_dbscan(eps=0.45, min_points=7, print_progress=False))
 
 max_label = labels.max()
 print(f"point cloud has {max_label + 1} clusters")
+## Get label colors
 colors = plt.get_cmap("tab20")(labels/(max_label if max_label>0 else 1))
 colors[labels<0] = 0
+## Colorized objects on the road
 outlier_cloud.colors = open3d.utility.Vector3dVector(colors[:, :3])
 open3d.visualization.draw_geometries([outlier_cloud])
 
@@ -42,8 +44,10 @@ indexes = pd.Series(range(len(labels))).groupby(labels, sort=False).apply(list).
 MAX_POINTS = 300
 MIN_POINTS = 40
 
+## For each individual object on the road
 for i in range(0, len(indexes)):
     nb_points = len(outlier_cloud.select_by_index(indexes[i]).points)
+    # If object size within the criteria, draw bounding box
     if (nb_points>MIN_POINTS and nb_points<MAX_POINTS):
         sub_cloud = outlier_cloud.select_by_index(indexes[i])
         obb = sub_cloud.get_axis_aligned_bounding_box()
@@ -52,6 +56,7 @@ for i in range(0, len(indexes)):
 
 print(f"Number of Bounding Boxes calculated {len(obbs)}")
 
+## Combined all visuals: outlier_cloud (objects), obbs (oriented bounding boxes), inlier_cloud (road)
 list_of_visuals = []
 list_of_visuals.append(outlier_cloud)
 list_of_visuals.extend(obbs)
